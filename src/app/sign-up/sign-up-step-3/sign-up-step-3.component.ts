68963189207e8b8afb3a0eea50045ec7b41f3cc2
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-10-14 18:12:24 +0700

    add step 2

--- src/app/sign-up/sign-up-step-3/sign-up-step-3.component.ts
+++ src/app/sign-up/sign-up-step-3/sign-up-step-3.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-sign-up-step-3',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CheckboxModule,
+  ],
   templateUrl: './sign-up-step-3.component.html',
   styleUrl: './sign-up-step-3.component.scss'
 })
 export class SignUpStep3Component {
+  @Output() nextStep: EventEmitter<void> = new EventEmitter<void>();
+  public emitNextStep(): void {
+    this.nextStep.emit();
+  }
 
+  form!: FormGroup;
+  ngOnInit() {
+    this.form = new FormGroup({
+      agree: new FormControl(false)
+    });
+  }
 }
