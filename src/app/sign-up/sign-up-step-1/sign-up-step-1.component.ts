efea9806494fd9510085ae1ef985959c7b855dd2
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-10-14 15:47:53 +0700

    add-step-1

--- src/app/sign-up/sign-up-step-1/sign-up-step-1.component.ts
+++ src/app/sign-up/sign-up-step-1/sign-up-step-1.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+
 
 @Component({
   selector: 'app-sign-up-step-1',
   standalone: true,
-  imports: [],
+  imports: [
+    PasswordModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './sign-up-step-1.component.html',
   styleUrl: './sign-up-step-1.component.scss'
 })
 export class SignUpStep1Component {
+  @Output() nextStep: EventEmitter<void> = new EventEmitter<void>();
+  @Output() previousStep: EventEmitter<void> = new EventEmitter<void>();
+
+  public emitNextStep(): void {
+    this.nextStep.emit();
+  }
+
+  public emitPreviousStep(): void {
+    this.previousStep.emit();
+  }
+
+  passwordForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      password: new FormControl('', Validators.required),
+    });
+
+  }
+
+  hasLetter(): boolean {
+    return /[a-zA-Z]/.test(this.passwordForm?.value);
+  }
+
+  hasNumberOrSpecialChar(): boolean {
+    return /[\d!@#$%^&*(),.?":{}|<>]/.test(this.passwordForm?.value);
+  }
+
+  isLongEnough(): boolean {
+    return this.passwordForm?.value?.length >= 10;
+  }
 
 }
