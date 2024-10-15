13d6c59e0cf23c7577b8d3a02508cf9fc04210fa
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-10-15 23:53:28 +0700

    fix deco

--- src/app/sign-up/sign-up-step-2/sign-up-step-2.component.ts
+++ src/app/sign-up/sign-up-step-2/sign-up-step-2.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
@@ -22,7 +23,8 @@ import { RadioButtonModule } from 'primeng/radiobutton';
     RadioButtonModule,
     ButtonModule,
     MessagesModule,
-    CommonModule
+    CommonModule,
+    CalendarModule
     
   ],
   templateUrl: './sign-up-step-2.component.html',
@@ -32,67 +34,23 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 export class SignUpStep2Component {
   @Output() nextStep: EventEmitter<void> = new EventEmitter<void>();
   public emitNextStep(): void {
-    if (this.infoForm.invalid) {
-      this.infoForm.markAllAsTouched();
-      return;
-    }
-
     this.nextStep.emit();
   }
   infoForm!: FormGroup;
 
   genders: any[] = [
-    { label: 'Nam', code: 'Male'},
-    { label: 'Nữ', code: 'Female'},
-    { label: 'Không phân biệt giới tính', code: 'Not'},
-    { label: 'Giới tính khác', code: 'Other'},
-    { label: 'Không muốn nêu cụ thể', code: 'None'}
+    { label: 'Male', code: 'Male'},
+    { label: 'Female', code: 'Female'},
+    { label: 'No-gender', code: 'No-gender'},
+    { label: 'Other', code: 'Other'},
+    { label: 'No-specify', code: 'No-specify'}
   ];
-  
-  months: any[] = [
-    { label: 'January', code: 1 },
-    { label: 'February', code: 2 },
-    { label: 'March', code: 3 },
-    { label: 'April', code: 4 },
-    { label: 'May', code: 5 },
-    { label: 'June', code: 6 },
-    { label: 'July', code: 7 },
-    { label: 'August', code: 8 },
-    { label: 'September', code: 9 },
-    { label: 'October', code: 10 },
-    { label: 'November', code: 11 },
-    { label: 'December', code: 12 }
-  ]
 
   ngOnInit() {
     this.infoForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      date: new FormControl('', [Validators.required, this.dayValidator, Validators.maxLength(2)]),
-      month: new FormControl('', Validators.required),
-      year: new FormControl('', [Validators.required, this.yearValidator1, this.yearValidator2, Validators.maxLength(4)]),
+      date: new FormControl('', [Validators.required]),
       gender: new FormControl('', Validators.required),
     });
   }
-
-  dayValidator(control: AbstractControl) {
-    if (control.value < 1 || control.value > 31) {
-      return { day: true };
-    }
-    return null;
-  }
-
-  yearValidator1(control: AbstractControl) {
-    if (control.value === 0 || control.value === null) {
-      return { yearLength: true };
-    }
-    return null;
-  }
-
-  yearValidator2(control: AbstractControl) {
-    if (control.value < 1900) {
-      return { year: true };
-    }
-    return null;
-  }
-
 }
