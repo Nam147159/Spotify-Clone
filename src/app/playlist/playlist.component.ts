48b65a35e94dbc3b7b56f81b2a614372a5fc796b
Nguyen Thanh Tuan <nguyentuan02156@gmail.com>
2024-11-28 10:11:26 +0700

    fix layout

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -4,9 +4,6 @@ import { DataViewModule } from 'primeng/dataview';
 import { ListboxModule } from 'primeng/listbox';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
-import { MainPanelService } from '../../services/main-panel-service/main-panel.service';
-import { Album } from '../models/spotify.model';
-
 
 @Component({
   selector: 'app-playlist',
@@ -17,25 +14,29 @@ import { Album } from '../models/spotify.model';
     ListboxModule,
     ButtonModule,
     CommonModule,
-    
   ],
-  providers: [MainPanelService],
+  providers: [],
   templateUrl: './playlist.component.html',
-  styleUrl: './playlist.component.scss'
+  styleUrl: './playlist.component.scss',
 })
 export class PlaylistComponent {
-  layout: string = 'list';
+  layout: string = `list`;
 
-  products!: Album[];
+  products!: any[];
 
-  constructor(
-    private testService: MainPanelService
-  ) {}
+  constructor() // private testService: MainPanelService
+  {}
 
   ngOnInit() {
-      this.testService.getPopularAlbums().subscribe((data: Album[]) => {
-          this.products = data;
-          console.log(this.products);
-      });
+    this.products = [
+      { image: 1, name: 2, category: 3, price: 4 },
+      { image: 1, name: 3, category: 4, price: 5 },
+      { image: 1, name: 4, category: 5, price: 6 },
+    ];
+    
+    // this.testService.getPopularAlbums().subscribe((data: any[]) => {
+    //     this.products = data;
+    //     console.log(this.products);
+    // });
   }
 }
