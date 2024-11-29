21ce1d9b30f677176a444f44f2057167d73bea8b
012e <huyphmnhat@gmail.com>
2024-11-29 22:09:27 +0700

    basic profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,50 @@
+<<<<<<< HEAD
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpMainComponent } from './sign-up/sign-up-main/sign-up-main.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+||||||| parent of dcf229a (basic profile page)
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpMainComponent } from './sign-up/sign-up-main/sign-up-main.component';
+=======
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { SignUpMainComponent } from "./sign-up/sign-up-main/sign-up-main.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { MainPanelComponent } from "./main-panel/main-panel.component";
+>>>>>>> dcf229a (basic profile page)
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: HomeComponent,
-    title: 'Spotify - Web Player: Music for everyone',
+    title: "Spotify - Web Player: Music for everyone",
+    children: [
+      {
+        path: "",
+        component: MainPanelComponent,
+        title: "Spotify - Web Player: Music for everyone",
+      },
+      {
+        path: "me",
+        component: UserProfileComponent,
+        title: "Spotify - Web Player: Music for everyone",
+      },
+    ],
   },
   {
-    path: 'sign-up',
+    path: "sign-up",
     component: SignUpMainComponent,
-    title: 'Spotify - Web Player: Music for everyone',
+    title: "Spotify - Web Player: Music for everyone",
   },
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent,
-    title: 'Spotify - Web Player: Music for everyone',
+    title: "Spotify - Web Player: Music for everyone",
   },
   {
     path: 'playlist',
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,13 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { LibraryPanelComponent } from "../library-panel/library-panel.component";
 import { MainPanelComponent } from "../main-panel/main-panel.component";
 import { SignUpBarComponent } from "../sign-up-bar/sign-up-bar.component";
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, LibraryPanelComponent, MainPanelComponent, SignUpBarComponent],
+  imports: [NavBarComponent, LibraryPanelComponent, MainPanelComponent, SignUpBarComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
