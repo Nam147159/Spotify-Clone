2b1facadb3b002a79e63956370c6c1d0cd4e67a2
Nam Nguyen <namnguyen147159@gmail.com>
2024-11-30 14:50:25 +0700

    Adjust homepage UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from '../services/api.service';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { SignUpBarComponent } from "./sign-up-bar/sign-up-bar.component";
+import { AuthenticationService } from '../services/authentication-service/authentication.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, SignUpBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
-  // message = '';
+export class AppComponent implements OnInit {
 
-  // constructor(private apiService: ApiService) {}
+  isLoggedIn: boolean = false;
+  constructor(private authService: AuthenticationService) {}
 
-  // ngOnInit() {
-  //   this.apiService.getMessage().subscribe((data: any) => {
-  //     this.message = data.message;
-  //     console.log(this.message);
-  //   });
-  // }
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.authService.getToken(); // Sử dụng hàm getToken để kiểm tra
+  }
 }
