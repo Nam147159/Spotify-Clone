2a582ac37828d4ccc5013c4fd4387bbec534d9d6
Nam Nguyen <namnguyen147159@gmail.com>
2024-11-30 16:43:55 +0700

    add basic player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ApiService } from '../services/api.service';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { SignUpBarComponent } from "./sign-up-bar/sign-up-bar.component";
 import { AuthenticationService } from '../services/authentication-service/authentication.service';
+import { PlayerComponent } from "./player/player.component";
+import { Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, SignUpBarComponent],
+  imports: [RouterOutlet, NavBarComponent, SignUpBarComponent, PlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent implements OnInit {
-
+  notSignupOrLogin: boolean = true;
   isLoggedIn: boolean = false;
-  constructor(private authService: AuthenticationService) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   ngOnInit(): void {
-    this.isLoggedIn = !!this.authService.getToken(); // Sử dụng hàm getToken để kiểm tra
+    this.isLoggedIn = !!this.authService.getToken();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isLoggedIn = !!this.authService.getToken();
+      const currentRoute = this.router.url;
+      this.notSignupOrLogin = !(currentRoute === '/sign-up' || currentRoute === '/login');
+    });
   }
 }
