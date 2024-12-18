6ca35a119ae5a6a8ce12f0ba1e67206e1b06f823
Nam Nguyen <namnguyen147159@gmail.com>
2024-12-18 16:14:34 +0700

    set up player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -5,6 +5,7 @@ import { ProgressBarModule } from 'primeng/progressbar';
 import { ButtonModule } from 'primeng/button';
 import { PrimeIcons } from 'primeng/api';
 import { FormsModule } from '@angular/forms';
+import { PlayerService } from '../../services/player-service/player.service';
 
 @Component({
   selector: 'app-player',
@@ -15,56 +16,38 @@ import { FormsModule } from '@angular/forms';
 })
 export class PlayerComponent {
 
+  constructor(private playerService: PlayerService) { }
 
+  private accessToken: string = "";
 
-  track = {
-    title: 'Snowman',
-    artist: 'Sia',
-    albumArt: 'https://via.placeholder.com/120', // Example album art
-  };
-
-  progress: number = 0;
-  volume: number = 50;
-  elapsedTime: number = 65; // Current elapsed time in seconds
-  totalTime: number = 165; // Total song time in seconds (2:45)
-
-  playing: boolean = false;
-  interval: any;
-
-  playPause() {
-    this.playing = !this.playing;
-    if (this.playing) {
-      this.startProgress();
-    } else {
-      clearInterval(this.interval);
-    }
-  }
-
-  startProgress() {
-    this.interval = setInterval(() => {
-      if (this.elapsedTime < this.totalTime) {
-        this.elapsedTime++;
-        this.progress = (this.elapsedTime / this.totalTime) * 100;
-      } else {
-        clearInterval(this.interval);
-        this.playing = false;
+  ngOnInit() {
+    this.playerService.getAccessToken().subscribe({
+      next: (response) => {
+        this.accessToken = response.token;  // Lưu token
+        this.playerService.initializePlayer(this.accessToken);  // Khởi tạo player với token
+      },
+      error: (error) => {
+        console.error('Error fetching token:', error);  // Xử lý lỗi nếu có
+      },
+      complete: () => {
+        console.log('Token request complete');  // Thực thi khi hoàn thành stream
       }
-    }, 1000);
+    });
   }
 
-  nextTrack() {
-    // Logic to switch to next track
-    this.track.title = 'Next Song';
-    this.track.artist = 'Next Artist';
-    this.elapsedTime = 0;
-    this.progress = 0;
+  play() {
+    this.playerService.play();
   }
 
-  previousTrack() {
-    // Logic to switch to previous track
-    this.track.title = 'Previous Song';
-    this.track.artist = 'Previous Artist';
-    this.elapsedTime = 0;
-    this.progress = 0;
+  pause() {
+    this.playerService.pause();
+  }
+
+  next() {
+    this.playerService.nextTrack();
+  }
+
+  previous() {
+    this.playerService.previousTrack();
   }
 }
