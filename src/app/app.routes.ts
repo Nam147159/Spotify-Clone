a4755c44ce4ae6eb717c753bdfb7f6be9d9499f4
Vuvom1 <21522808@gm.uit.edu.vn>
2024-12-27 23:39:20 +0700

    Create Browse Panel And Add Sub Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,25 @@ import { LoginComponent } from './login/login.component';
 import { SignUpMainComponent } from './sign-up/sign-up-main/sign-up-main.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { AlbumnInfoComponent } from './albumn-info/albumn-info.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
+import { BrowsePanelComponent } from './browse-panel/browse-panel.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: 'Spotify - Web Player: Music for everyone'
+    title: 'Spotify - Web Player: Music for everyone',
+    children: [
+      {
+        path: 'home',
+        component: MainPanelComponent,
+      },
+      {
+        path: 'search',
+        component: BrowsePanelComponent,
+        title: 'Spotify - Playlist Details'
+      }
+    ]
   },
   {
     path: 'sign-up',
@@ -24,11 +37,11 @@ export const routes: Routes = [
   {
     path: 'playlist',
     component: PlaylistComponent,
-    title: 'Spotify - Web Player: Music for everyone'
+    title: 'Spotify - Web Player: Music for everyone',
   },
   {
     path: 'album/:id',
     component: AlbumnInfoComponent,
     title: 'Spotify - Web Player: Music for everyone'
-  }
+  },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,16 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { LibraryPanelComponent } from "../library-panel/library-panel.component";
 import { MainPanelComponent } from "../main-panel/main-panel.component";
 import { SignUpBarComponent } from "../sign-up-bar/sign-up-bar.component";
+import { BrowsePanelComponent } from "../browse-panel/browse-panel.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, LibraryPanelComponent, MainPanelComponent, SignUpBarComponent],
+  imports: [RouterOutlet, NavBarComponent, LibraryPanelComponent, MainPanelComponent, SignUpBarComponent, BrowsePanelComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
 }
